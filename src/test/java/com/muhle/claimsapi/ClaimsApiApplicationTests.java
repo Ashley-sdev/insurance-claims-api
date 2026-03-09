cd35197560d3c73c95ff2267d5cf5d75c8926752
Ashley-sdev <muhlentuli0223@gmail.com>
2026-03-09 23:18:43 +0200

    Phase 0: Initial Spring Boot project setup with Gradle, JPA, and H2 database

---
+++ src/test/java/com/muhle/claimsapi/ClaimsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhle.claimsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
